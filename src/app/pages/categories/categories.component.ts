7c0876445be590a3a2384f6c18087feb0d0d8d1c
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2025-04-21 19:03:17 -0500

    Creación de componente Categories

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,15 +1,82 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CategoriesService } from '../../core/services/categories.service';
+import { FormCategoriesComponent } from './form-categories/form-categories.component';
+import { CategoriesActionsComponent } from './categories-actions/categories-actions.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
-  imports: [CommonModule, NgbAlertModule],
-  templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgbModalModule, CategoriesActionsComponent],
+  templateUrl: './categories.component.html'
 })
 export class CategoriesComponent {
-  dismissible = true;
+  title = 'Gestión de Categorías';
+  subtitle = 'Administra las categorías disponibles en el sistema';
 
-  constructor() {}
+  columns = [
+    { title: 'ID', property: 'id', type: 'text' },
+    { title: 'Nombre', property: 'name', type: 'text' },
+    { title: 'Creado el', property: 'created_at', type: 'text' }
+  ];
+
+  data: any[] = [];
+  search: string = '';
+  page = 1;
+  perPage = 5;
+  totalItems = 0;
+  timer: any;
+
+  constructor(private service: CategoriesService, private modal: NgbModal) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    const params: any = {
+      page: this.page,
+      per_page: this.perPage
+    };
+
+    if (this.search.length >= 3) {
+      params.search = this.search;
+    }
+
+    this.service.getAll('categories', params).subscribe((res: any) => {
+      this.data = res.data;
+    });
+  }
+
+  openForm() {
+    const ref = this.modal.open(FormCategoriesComponent);
+    ref.componentInstance.type = 'create';
+    ref.componentInstance.data = null;
+    ref.componentInstance.saved = this.loadData.bind(this);
+  }
+
+  onSearch(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.search = value;
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      if (this.search.length >= 3 || this.search.length === 0) {
+        this.page = 1;
+        this.loadData();
+      }
+    }, 1000);
+  }
+
+  changePerPage(value: number) {
+    this.perPage = value;
+    this.page = 1;
+    this.loadData();
+  }
+
+  changePage(value: number) {
+    this.page = value;
+    this.loadData();
+  }
 }
